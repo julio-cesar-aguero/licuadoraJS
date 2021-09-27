32cc974047e53429b8f0697d82f07631ecece1a9
julio-cesar-aguero <julio.aguero.gomez@gmail.com>
2021-09-27 18:30:37 -0500

    licuadoraJS-final

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 console.log("LICUADORAJS 1.0");
 let estadoLicuadora = "apagada";
 let licuadoraButton = document.getElementById("blender");
+let sonidoLicuadora = document.getElementById("blender-sound");
+let botonLicuadora = document.getElementById("blender-button-sound");
 
 const licuadora = () =>{
     if(estadoLicuadora =="apagada"){
         estadoLicuadora = "encendido";
+        licuar();
         licuadoraButton.classList.add("blender--active")
         console.log("Encendido");
+        
     }else{
         estadoLicuadora = "apagada"
+        licuar();
         licuadoraButton.classList.remove("blender--active")
         console.log("Apagado");
+        
     }
 }
 
+const licuar = () =>{
+    if (sonidoLicuadora.paused) {
+        botonLicuadora.play();
+        sonidoLicuadora.play();
+    } else {
+        botonLicuadora.play();
+        sonidoLicuadora.pause();
+        sonidoLicuadora.currentTime = 0;
+    }
+}
